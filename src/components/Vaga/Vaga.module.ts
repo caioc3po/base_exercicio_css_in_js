e2c194ec1b8f81da304db4c723d722e6b43f6c59
caioc3po <caioc3po@gmail.com>
2025-05-15 17:30:44 -0300

    concertando o erro de digitação no styled component de Vaga

--- src/components/Vaga/Vaga.module.ts
+++ src/components/Vaga/Vaga.module.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 
-export const VagaCard = styled.l1`
+export const VagaCard = styled.li`
   border: 1px solid var(--cor-principal);
   background-color: var(--cor-secundaria);
   color: var(--cor-principal);
