7c156a77d97c62f9a4e335cd90369dc6bdd23cfd
caioc3po <caioc3po@gmail.com>
2025-05-19 18:10:14 -0300

    fix: importação do estilo global

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global'
+import GlobalStyle from './global'
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Hero />
       <div className="container">
